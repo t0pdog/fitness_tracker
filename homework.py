db02f7c34a1dd0e54e30141bc00f78ce962bcbc1
t0pdog <topillustrated@gmail.com>
2022-04-03 23:32:23 +0300

    2 review

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict
 
 
 @dataclass
@@ -52,9 +52,9 @@ class Training:
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
 
-        # Нужна ли эта проверка?
-        # if type(self).__name__ == None:
-        #     return InfoMessage(0, 0, 0, 0, 0)
+        # Проверка 'if type(self).__name__ == None' не нужна,
+        # так как в read_package уже есть
+        # проверка 'if workout_type in trainings'
 
         return InfoMessage(
             type(self).__name__,
@@ -64,42 +64,32 @@ class Training:
             self.get_spent_calories()
         )
 
-        # Этот вариант, как я понял неудобен с точки зрения расширяемости:
-        # if (type(self).__name__ == 'Running' or
-        #     'SportsWalking' or 'Swimming'):
-        #     return InfoMessage(
-        #         type(self).__name__,
-        #         self.duration,
-        #         self.get_distance(),
-        #         self.get_mean_speed(),
-        #         self.get_spent_calories()
-        #     )
-        # else:
-        #     return InfoMessage(0, 0, 0, 0, 0)
-
 
 class Running(Training):
     """Тренировка: бег."""
+    COEFF_CALOR_1: int = 18
+    COEFF_CALOR_2: int = 20
+    MINS_IN_HOUR: int = 60
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        coeff_calor_1 = 18
-        coeff_calor_2 = 20
-        coeff_m_in_hr = 60
 
         return (
-            (coeff_calor_1
+            (self.COEFF_CALOR_1
              * self.get_mean_speed()
-             - coeff_calor_2)
+             - self.COEFF_CALOR_2)
             * self.weight
             / self.M_IN_KM
             * self.duration
-            * coeff_m_in_hr
+            * self.MINS_IN_HOUR
         )
 
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
+    COEFF_CAL_1:float = 0.035
+    COEFF_CAL_2:float = 0.029
+    MINS_IN_HOUR: int = 60
 
     def __init__(self,
                  action: int,
@@ -112,25 +102,24 @@ class SportsWalking(Training):
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        coeff_cal_1 = 0.035
-        coeff_cal_2 = 0.029
-        coeff_m_in_hr = 60
 
         return (
-            (coeff_cal_1
+            (self.COEFF_CAL_1
              * self.weight
              + (self.get_mean_speed()**2
                 // self.height)
-             * coeff_cal_2
+             * self.COEFF_CAL_2
              * self.weight)
             * self.duration
-            * coeff_m_in_hr
+            * self.MINS_IN_HOUR
         )
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
     LEN_STEP: float = 1.38
+    COEFF_CAL_1: float = 1.1
+    COEFF_CAL_2: int = 2
 
     def __init__(self,
                  action: int,
@@ -155,20 +144,18 @@ class Swimming(Training):
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        coeff_cal_1 = 1.1
-        coeff_cal_2 = 2
 
         return (
             (self.get_mean_speed()
-             + coeff_cal_1)
-            * coeff_cal_2
+             + self.COEFF_CAL_1)
+            * self.COEFF_CAL_2
             * self.weight
         )
 
 
 def read_package(workout_type: str, data: List[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    trainings = {
+    trainings: Dict[str, Training] = {
         'SWM': Swimming,
         'RUN': Running,
         'WLK': SportsWalking
@@ -176,6 +163,8 @@ def read_package(workout_type: str, data: List[int]) -> Training:
 
     if workout_type in trainings:
         return trainings[workout_type](*data)
+    else:
+        raise Warning("workout_type is invalid!")
 
 
 def main(training: Training) -> None:
@@ -186,7 +175,7 @@ def main(training: Training) -> None:
 
 
 if __name__ == '__main__':
-    packages = [
+    packages:Dict[str, int] = [
         ('SWM', [720, 1, 80, 25, 40]),
         ('RUN', [15000, 1, 75]),
         ('WLK', [9000, 1, 75, 180]),
